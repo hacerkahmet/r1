f34e7041115850a05735e162771343202a3804d6
hacerkahmet <hacerkucukahmet@gmail.com>
2020-09-27 01:40:58 +0300

    second commit

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebApplication1.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your applicationss descriptio nfff page.";
+            ViewBag.Message = "Your applicationssggg descriptio nfff page.";
 
             return View();
         }
